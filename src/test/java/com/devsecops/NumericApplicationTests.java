d3c4843d5d79c40b68ce2979c4705c05b74ca82a
Cuncis <karang.reksa@gmail.com>
2021-12-29 09:50:58 +0700

    Update NumericApplicationTests.java

--- src/test/java/com/devsecops/NumericApplicationTests.java
+++ src/test/java/com/devsecops/NumericApplicationTests.java
@@ -48,8 +48,7 @@ public class NumericApplicationTests {
     
     @Test
     public void welcomeMessage() throws Exception {
-         this.mockMvc.perform(get("/")).andDo(print()).addExpect(status().isOk())   
-            .andExpect(content().string("Kubernetes DevSecOps"));
+         this.mockMvc.perform(get("/")).andDo(print()).addExpect(status().isOk());
     }
     
 
